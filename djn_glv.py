8318cf6f9eb09aae43a982dfdc3f3653381b6386
Nowacki <dnowacki@usgs.gov>
2025-03-21 12:55:31 -0700

    glv updates for use on windows machine

--- djn_glv.py
+++ djn_glv.py
@@ -11,8 +11,6 @@ from rectifier_crs import Rectifier, TargetGrid
 import pandas as pd
 from joblib import Parallel, delayed
 import xarray as xr
-fildir = '/Volumes/Argus/glv/'
-
 
 # water level for 2021 data
 # rbr = xr.load_dataset('/Users/dnowacki/Library/CloudStorage/OneDrive-DOI/Alaska/brw2021/rbr/041230_20211007_2113/wave_interval_512/brwrbrs-a.nc')
@@ -30,8 +28,12 @@ n9468333['wl'] = n9468333['water_level']
 
 USE_GNSS = True
 gnss = xr.load_dataset('/Users/dnowacki/OneDrive - DOI/Alaska/gnssr/glv0_model2.nc')
+USE_SPLINE = True
+gnss = xr.load_dataset('/Users/dnowacki/OneDrive - DOI/Alaska/gnssr/glv0_model2_spline.nc')
 
 # %%
+fildir = '/Volumes/Argus/glv/'
+fildir = 'd:' + fildir
 camera = 'cx'
 product = 'snap'
 
@@ -84,18 +86,13 @@ def lazyrun(metadata, intrinsics_list, extrinsics_list, local_origin, t, z):
         print("No wl data for", pd.to_datetime(int(t), unit='s'))
         return
     """ coordinate system setup"""
-    if product == 'dark':
-        dx = 0.1
-        dy = 0.1
-    else:
-        dx = 0.1
-        dy = 0.1
+    dx = 0.1
+    dy = 0.1
 
     xmin = 0
     xmax = 300-dx
     ymin = 0
     ymax = 250-dy
-
     # z = 0 # z is now defined by water level input
     print(f'***Z: {z} m NAVD88')
     rectifier_grid = TargetGrid(
@@ -108,7 +105,6 @@ def lazyrun(metadata, intrinsics_list, extrinsics_list, local_origin, t, z):
 
     rectifier = Rectifier(rectifier_grid)
 
-    fildir = '/Volumes/Argus/glv/'
     if camera == 'cx':
         c1bad = False
         c2bad = False
@@ -117,7 +113,7 @@ def lazyrun(metadata, intrinsics_list, extrinsics_list, local_origin, t, z):
         # print(image_files)
         try:
             c1ref = skimage.io.imread(image_files[0])
-        except AttributeError:
+        except (AttributeError, ValueError, OSError):
             print('could not process', image_files[0], '; RETURNING')
             return
         except FileNotFoundError:
@@ -127,7 +123,7 @@ def lazyrun(metadata, intrinsics_list, extrinsics_list, local_origin, t, z):
 
         try:
             c2src = skimage.io.imread(image_files[1])
-        except AttributeError:
+        except (AttributeError, ValueError, OSError):
             print('could not process', image_files[1], '; RETURNING')
             return
         except FileNotFoundError:
@@ -141,20 +137,24 @@ def lazyrun(metadata, intrinsics_list, extrinsics_list, local_origin, t, z):
 
     print(f"{c1bad=}, {c2bad=}")
 
+    if c1bad and c2bad:
+        print('both bad, returning')
+        return
+
     if c1bad:
         intrinsics_list = [intrinsics_list[1]]
         extrinsics_list = [extrinsics_list[1]]
+        image_files = [image_files[1]]
     elif c2bad:
         intrinsics_list = [intrinsics_list[0]]
         extrinsics_list = [extrinsics_list[0]]
+        image_files = [image_files[0]]
 
-    try:
-        rectified_image = rectifier.rectify_images(metadata, image_files, intrinsics_list, extrinsics_list, local_origin)
-    except FileNotFoundError:
-        print("could not find", image_files)
-        return
-    if USE_GNSS:
+    rectified_image = rectifier.rectify_images(metadata, image_files, intrinsics_list, extrinsics_list, local_origin)
+    if USE_GNSS and not USE_SPLINE:
         ofile = fildir + 'rect/gnssr/' + product + '/' + t + pd.Timestamp(int(t), unit='s').strftime('.%a.%b.%d_%H_%M_%S.GMT.%Y.golovin') + '.' + camera + '.' + product + '.png'
+    elif USE_GNSS and USE_SPLINE:
+        ofile = fildir + 'rect/gnssr_spline/' + product + '/' + t + pd.Timestamp(int(t), unit='s').strftime('.%a.%b.%d_%H_%M_%S.GMT.%Y.golovin') + '.' + camera + '.' + product + '.png'
     else:
         ofile = fildir + 'rect/' + product + '/' + t + pd.Timestamp(int(t), unit='s').strftime('.%a.%b.%d_%H_%M_%S.GMT.%Y.golovin') + '.' + camera + '.' + product + '.png'
     print(ofile)
@@ -164,8 +164,7 @@ def lazyrun(metadata, intrinsics_list, extrinsics_list, local_origin, t, z):
     # ofile = fildir + 'proc/rect/' + t + '.' + camera + '.' + product + '.rect.matched.png'
     # imageio.imwrite(ofile, np.flip(rectified_image_matched, 0), format='png', optimize=True)
 
-# ts1 = [os.path.basename(x).split('.')[0] for x in glob.glob('/Volumes/Backstaff/field/unk/products/163[0-9][3-9][0-9][6-9]*c1.'+ product + '.jpg')]
-# ts2 = [os.path.basename(x).split('.')[0] for x in glob.glob('/Volumes/Backstaff/field/unk/products/163[0-9][3-9][0-9][6-9]*c2.' + product + '.jpg')]
+    return rectifier
 
 ts1 = [os.path.basename(x).split('.')[0] for x in glob.glob(fildir + 'products/1*c1.'+ product + '.jpg')]
 ts2 = [os.path.basename(x).split('.')[0] for x in glob.glob(fildir + 'products/1*c2.' + product + '.jpg')]
@@ -177,15 +176,19 @@ elif camera == 'c2':
 elif camera == 'cx':
     ts = list(set(ts1) | set(ts2)) # use OR instead of AND so we can have rectified images when just one camera was active
 
-ts  = [x for x in ts if int(x) >= 1630294200 ] # only good images from when the camera was aimed correctly
+if USE_GNSS and not USE_SPLINE:
+    shim = 'gnssr/'
+elif USE_GNSS and USE_SPLINE:
+    shim = 'gnssr_spline/'
+else:
+    shim = ''
+with open(fildir + 'rect/' + shim + product + '/done.txt', 'w') as f:
+    for g in glob.glob(fildir + 'rect/' + shim + product + '/*png'):
+        f.write(f"{g.split('/')[-1].split("\\")[-1].split('.')[0]}\n")
+    for g in glob.glob(fildir + 'rect/' + shim + product + '/dark/*png'):
+        f.write(f"{g.split('/')[-1].split("\\")[-1].split('.')[0]}\n")
 
-with open(fildir + 'rect/' + product + '/done.txt', 'w') as f:
-    for g in glob.glob(fildir + 'rect/' + product + '/*png'):
-        f.write(f"{g.split('/')[-1].split('.')[0]}\n")
-    for g in glob.glob(fildir + 'rect/' + product + '/dark/*png'):
-        f.write(f"{g.split('/')[-1].split('.')[0]}\n")
-
-with open(fildir + 'rect/' + product + '/done.txt') as f:
+with open(fildir + 'rect/' + shim + product + '/done.txt') as f:
     tsdone = [line.rstrip() for line in f]
 
 # this will get what remains to be done
@@ -210,11 +213,10 @@ ts = tsnew
 print('***', len(ts))
 print(product, camera)
 
-
 ds = xr.Dataset()
 ds['time'] = xr.DataArray(pd.to_datetime([int(x) for x in ts], unit='s'), dims='time')
 ds['timestamp'] = xr.DataArray(ts, dims='time')
-if USE_GNSS is True:
+if USE_GNSS:
     ds['wl'] = gnss['wl'].reindex_like(ds['time'], method='nearest', tolerance='60min')
 else:
     ds['wl'] = n9468333['water_level'].reindex_like(ds['time'], method='nearest', tolerance='10min')
@@ -224,32 +226,19 @@ ds = ds.sortby('time')
 import random
 random.shuffle(ts)
 
-# %%
-print(product, camera)
-# t = ts[0]
-# n9468333['v'][np.argmin(np.abs(pd.DatetimeIndex(n9468333.time.values) - pd.to_datetime(t, unit='s')))].values
-Parallel(n_jobs=-4)(
-    delayed(lazyrun)(
-        metadata, intrinsics_list, extrinsics_list, local_origin, t,
-        ds['wl'][ds.timestamp == t].values)
-        for t in ts
-        )
-# %%
+        
+import multiprocessing as mp
+def split_into_chunks(lst, chunk_size):
+    result = []
+    for i in range(0, len(lst), chunk_size):
+        result.append(lst[i:i + chunk_size])
+    return result
+    
+print(len(ts))
 
-# [lazyrun(metadata, intrinsics_list, extrinsics_list, local_origin, t, 1) for t in ts]
-for t in ts[0:2]:
-    print(t)
-    try:
-
-        lazyrun(metadata,
-            intrinsics_list,
-            extrinsics_list,
-            local_origin,
-            t,
-            ds['wl'][ds.timestamp == t].values
-            # 1, # set z to 1, which is the approx water elevation from the beach survey
-        )
-    except KeyError:
-        print("No wl data for", pd.to_datetime(t, unit='s'))
-    except FileNotFoundError:
-        print("No file found for ", pd.to_datetime(t, unit='s'))
+if __name__ == '__main__':
+    for tsshort in split_into_chunks(ts, 200):
+        with mp.Pool(processes=10,maxtasksperchild=10) as pool:
+            result = pool.starmap(lazyrun, [(
+                metadata, intrinsics_list, extrinsics_list, local_origin, t,
+                ds['wl'][ds.timestamp == t].values) for t in tsshort])
