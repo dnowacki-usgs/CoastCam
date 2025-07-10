e016a19ab32287d5d1543ddffdc1f36c61d69797
Dan Nowacki <dnowacki@usgs.gov>
2025-07-09 18:34:32 -0700

    ruff stuff

--- djn_brw.py
+++ djn_brw.py
@@ -9,8 +9,9 @@ from coastcam_funcs import json2dict
 from calibration_crs import CameraCalibration
 from rectifier_crs import Rectifier, TargetGrid
 import pandas as pd
-from joblib import Parallel, delayed
 import xarray as xr
+import multiprocessing as mp
+import random
 
 # water level for 2021 data
 # rbr = xr.load_dataset('/Users/dnowacki/Library/CloudStorage/OneDrive-DOI/Alaska/brw2021/rbr/041230_20211007_2113/wave_interval_512/brwrbrs-a.nc')
@@ -108,7 +109,7 @@ def lazyrun(metadata, intrinsics_list, extrinsics_list, local_origin, t, z):
                        fildir + 'products/' + t + '.c2.' + product + '.jpg']
         # print(image_files)
         try:
-            c1ref = skimage.io.imread(image_files[0])
+            skimage.io.imread(image_files[0])
         except (AttributeError, ValueError, OSError):
             print('could not process', image_files[0], '; RETURNING')
             return
@@ -118,7 +119,7 @@ def lazyrun(metadata, intrinsics_list, extrinsics_list, local_origin, t, z):
             c1bad = True
 
         try:
-            c2src = skimage.io.imread(image_files[1])
+            skimage.io.imread(image_files[1])
         except (AttributeError, ValueError, OSError):
             print('could not process', image_files[1], '; RETURNING')
             return
@@ -231,10 +232,10 @@ else:
 ds = ds.sortby('time')
 
 # randomize ts
-import random
+
 random.shuffle(ts)
 
-import multiprocessing as mp
+
 def split_into_chunks(lst, chunk_size):
     result = []
     for i in range(0, len(lst), chunk_size):
